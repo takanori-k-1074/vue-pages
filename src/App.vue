9cad0872bd288e9f84eb500e149615e3779f9775
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-12-11 16:29:10 +0900

    markup header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+      <div class="container clear">
+        <div class="header-left">
+          <h1 class="header-title">Takanori K's Portoforio</h1>
+        </div>
+        <div class="header-right">
+          <ul class="header-nav clear">
+            <li class="header-nav-item">
+              <router-link to="/">Home</router-link>
+            </li>
+            <li class="header-nav-item">
+              <router-link to="/about">About</router-link>
+            </li>
+            <li class="header-nav-item"><a href="#">favorite</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style></style>
