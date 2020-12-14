152d3089aac633c4fde8381073fd8fae38632a30
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-12-14 16:10:01 +0900

    home写真、リンクの追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,14 @@
       <div class="container clear">
         <h2 class="section-title">プロフィール</h2>
         <div class="profile-left">
-          <img src="#" />
+          <img src="../assets/image0.png" />
         </div>
         <div class="profile-right">
           <p>
             小泉高規(コイズミタカノリ)<br />
             自衛官退職後webエンジニアになるため本格的に学習開始<br />
-            ポートフォリオを作成しつつ就職活動しながら継続して不足していると考えられる技術について学習中
+            ポートフォリオを作成しつつ就職活動しながら継続して不足していると考えられる技術について学習中<br /><br />
+            このページはvue/cli 4.1.1で作成
           </p>
         </div>
       </div>
@@ -21,18 +22,28 @@
         <h2 class="section-title">ポートフォリオ一覧</h2>
         <div class="course-wrapper">
           <div class="course-item">
-            <img src="#" />
+            <a href="https://aqueous-beyond-99235.herokuapp.com/"
+              ><img src="../assets/portforio.png"
+            /></a>
+
             <p>Rails / Vue</p>
           </div>
           <div class="course-item">
-            <img src="#" />
-            <p>Rails / AWS / Docker</p>
+            <a href="https://mysteamnews1074.com/"
+              ><img src="../assets/steamnews.png" />
+            </a>
+            <p>Rails / AWS / Docker / steamAPI / Azuretranslator</p>
           </div>
           <div class="course-item">
-            <img src="#" />
-            <p>Ruby</p>
+            <img src="../assets/koizumibot1.png" />
+            <p>Ruby / LineDevelopers / openweathermap API</p>
           </div>
         </div>
+        <h3 class="section-title">
+          <a href="https://github.com/takanori-k-1074" class="link"
+            >githubのリンク</a
+          >
+        </h3>
       </div>
     </section>
   </div>
