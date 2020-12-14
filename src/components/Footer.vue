080246bd0e52d58ca048531d672788de04c4dc28
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-12-14 18:30:11 +0900

    footerデザイン修正

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>aaaaaaaaaaaaaaa</div>
+  <footer>
+    <div class="container clear">
+      <p>
+        <span>Takanori K's Portfolio</span> by
+        <a href="https://twitter.com/tnxdvl1074" class="footer-link">@tnxdvl1074</a>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
