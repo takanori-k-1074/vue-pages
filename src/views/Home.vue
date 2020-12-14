9231cede178a9d86cfbe9cb5a01b86d85d5215fb
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-12-14 15:14:26 +0900

    home枠作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>a</div>
+  <div>
+    <section class="profile section">
+      <div class="container clear">
+        <h2 class="section-title">プロフィール</h2>
+        <div class="profile-left">
+          <img src="#" />
+        </div>
+        <div class="profile-right">
+          <p>
+            小泉高規(コイズミタカノリ)<br />
+            自衛官退職後webエンジニアになるため本格的に学習開始<br />
+            ポートフォリオを作成しつつ就職活動しながら継続して不足していると考えられる技術について学習中
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="course section">
+      <div class="container">
+        <h2 class="section-title">ポートフォリオ一覧</h2>
+        <div class="course-wrapper">
+          <div class="course-item">
+            <img src="#" />
+            <p>Rails / Vue</p>
+          </div>
+          <div class="course-item">
+            <img src="#" />
+            <p>Rails / AWS / Docker</p>
+          </div>
+          <div class="course-item">
+            <img src="#" />
+            <p>Ruby</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
