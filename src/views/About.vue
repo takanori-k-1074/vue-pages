6a0e61eb61a0d31939268be1f8e94c63650d7839
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-12-18 13:15:27 +0900

    openwheatherapi読み込み

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,39 +1,62 @@
 <template>
-  <div id="search-ip">
-    <div>
-      <div>
-        <p>https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=262060&count=1&format=json</p>
-        <!-- <input @click="getIp" type="button" value="IPを取得"> -->
+  <div class="weather-container">
+    <div class="weather-wrap">
+      <div class="search-box">
+      <input type="text" placeholder="Search..." class="search-bar" 
+          v-model="query" v-on:keypress="fetchWeather" />
+      </div>
+      <div class="weather-info" v-if="typeof weather.main!='undefined'">
+        <div class="location-box">
+          <div class="location">{{weather.name}},{{weather.sys.country }}</div>
+          <div class="date">{{ todaysDate() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+          <!-- <div class="icon">
+                <img:src="`${weather_icon}${weather.weather[0].icon}${'@2x.png'}`"/>
+          </div> -->
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import axios from 'axios'
-
+import axios from "axios";
 export default {
-  name: 'BitcoinAPI',
-
-//API格納用のプロパティ
-  data: function(){
-    return{
-      data:{
-        appnews: null
-      }
-    }
+  data() {
+    return {
+      api_key: "",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      weather_icon: "http://openweathermap.org/img/wn/",
+      query: "",
+      weather: {},
+    };
   },
-
-//axiosによるデータ取得処理
-  mounted: function(){
-    axios.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json')
-    .then(function(response){
-        //デバッグ用にconsoleに出力
-        console.log(response.data.appnews)
-        this.appnews = response.data.appnews
-    }.bind(this))
-    .catch(function(error){
-        console.log(error)
-    })
-}
-}  
+  methods: {
+    async fetchWeather(e) {
+      if (e.key == "Enter") {
+      let response = await axios.get(
+      `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+      );
+      this.setResults(response.data);
+      }
+    },
+    setResults(returnedResponse) {
+    this.weather = returnedResponse;
+    },
+    todaysDate() {
+      const months = [
+      "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov",
+      "Dec",];
+      const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      let d = new Date();
+      let month = months[d.getMonth()];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let year = d.getFullYear();
+      return `${month} ${date} ${day} ${year}`;
+    },
+  },
+};
 </script>
