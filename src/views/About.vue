d76cb2565920ccbf1f6b149286e2fc30f2d9d98d
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2021-01-03 16:06:49 +0900

    インデント修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       {{ todaysDate() }}
     </div>
     <div class="weather">
-      <p>{{ query.data.data.weather[0].main}}</p>
+      <p>{{ query.data.data.weather[0].main }}</p>
       <p>{{ query.data.data.main.temp }}℃</p>
       <p>風速{{ query.data.data.wind.speed }}m</p>
       <p>{{ query.data.data.name }}</p>
@@ -25,17 +25,17 @@ export default {
       .get('/api/v1/weather')
       .then(response => (this.query = response));
   },
-  methods:{
+  methods: {
     todaysDate() {
-      const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",];
-      const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',];
+      const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
       let d = new Date();
       let month = months[d.getMonth()];
       let day = days[d.getDay()];
       let date = d.getDate();
       let year = d.getFullYear();
       return `${month} ${date} ${day} ${year}`;
-    },  
+    },
   },
 };
 </script>
