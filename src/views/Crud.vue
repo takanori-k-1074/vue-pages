4ec669fd5f61a54b699e026e47f9f6fc4556ec24
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2021-01-23 21:43:28 +0900

    デザイン

---
+++ src/views/Crud.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1 class="title">music~</h1>
+    <div class="">
+      <div class="col s4 m6">
+        <div class="card btn">
+          <span class="card-title">
+            music-title
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="row" >
+      <div class="col s12 m12">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">
+             a
+            </span>
+            <div class="detail">
+             b
+            </div>
+            <div class="detail">
+             c
+            </div>
+            <div class="detail">
+              d
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
