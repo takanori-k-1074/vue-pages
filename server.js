9c7dffcc1d77a71d33cda2875b4a6c6f56bc91ff
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2021-02-07 10:33:42 +0900

    twitter埋め込み用

--- server.js
+++ server.js
@@ -7,17 +7,19 @@ const app = express();
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true,
+  })
+);
 app.use(staticFileMiddleware);
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render(path.join(__dirname + '/dist/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
-  console.log("App now running on port", port);
+  console.log('App now running on port', port);
 });
