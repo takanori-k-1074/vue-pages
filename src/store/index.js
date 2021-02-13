972d9051c856a7c3ae81fd5921319450b2d7b32f
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2021-02-13 20:19:22 +0900

    vuexへの以降準備

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    // musics: []
+  },
+  // mutations: {
+  //   fetchMusics() {
+  //     axios.get(process.env.VUE_APP_API_URL_INDEX).then(
+  //       res => {
+  //         for (var i = 0; i < res.data.musics.length; i++) {
+  //           this.musics.push(res.data.musics[i]);
+  //         }
+  //       },
+  //       error => {
+  //         console.log(error);
+  //       }
+  //     );
+  //   },
+  // },
   actions: {},
   modules: {},
 });
